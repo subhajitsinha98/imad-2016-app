517fe59724cc7455ca67a681423e7e91d87b8f1d
Subhajit Sinha <subhajitsinha98@gmail.com>
2016-10-29 15:30:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/about', function (req, res) {
+app.get('/about', function (req, res) {
   res.send('article one is coming soon');
 });
 
